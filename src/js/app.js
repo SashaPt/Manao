4bfbeecc9604aa89dc977cb505e06f2819626a38
SashaPt <aleksa.toc18@gmail.com>
2024-04-28 14:15:18 +0300

    fix: change menu-links click behavior

--- src/js/app.js
+++ src/js/app.js
@@ -7,9 +7,10 @@ import { swiperInit } from './modules/swiperInit.js';
 const onLoad = () => {
   const modal = document.querySelector('.pop-up');
   const modalBtns = document.querySelectorAll('.pop-up__close');
-  const menu = document.querySelector('.header__nav-wrapper')
-  const menuBtns = document.querySelectorAll('.header__nav-top .close, .burger')
+  const menu = document.querySelector('.header__nav-wrapper');
+  const menuBtns = document.querySelectorAll('.header__nav-top .close, .burger');
   const form = document.querySelector('form');
+  const menuLinks = document.querySelectorAll('.header__menu-item a');
 
   flsFunctions.isWebp();
   handleScroll();
@@ -17,6 +18,8 @@ const onLoad = () => {
   handleForm(form);
   popUp(modal, modalBtns);
   popUp(menu, menuBtns);
+
+  menuLinks.forEach((link) => link.addEventListener('click', () => menu.classList.remove('visible')));
 };
 
 window.addEventListener('DOMContentLoaded', onLoad);
